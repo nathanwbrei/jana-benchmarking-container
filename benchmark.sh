a97afba212a701e9fe99745eddcc7b89aa26b9fc
Nathan Brei <nbrei@jlab.org>
2022-01-21 11:00:47 -0500

    Script produces the 3x4 numa matrix

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-jana -b -Pplugins=JTest -Pbenchmark:resultsdir=/app/data
+for AFFINITY in {0..2}
+do
+    for LOCALITY in {0..3}
+    do
+        mkdir /app/data/a${AFFINITY}l${LOCALITY}
+        jana -b -Pplugins=JTest -Pbenchmark:resultsdir=/app/data/a${AFFINITY}l${LOCALITY} -Pjana:affinity=$AFFINITY -Pjana:locality=$LOCALITY
+        if [ $? -ne 0 ]; then
+          exit
+        fi
+    done
+done
 
 
